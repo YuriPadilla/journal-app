2927fd481f454c84c37a37565bad3817f685d278
Yuri Padilla <yuripadilla017@gmail.com>
2022-12-28 17:59:28 +0100

    eliminate unnecessari files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 
 import Header from "./components/Header";
-import Main from "./components/Main";
 import Footer from "./components/Footer";
 import Form from "./components/Form";
 
