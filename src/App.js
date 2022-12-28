8cfbc2fb7f8b3c9ecfd545fa6774dc90061944c8
Yuri Padilla <yuripadilla017@gmail.com>
2022-12-28 17:49:26 +0100

    create journal-app in github

--- src/App.js
+++ src/App.js
@@ -6,34 +6,14 @@ import Main from "./components/Main";
 import Footer from "./components/Footer";
 import Form from "./components/Form";
 
-function App() {
+export default function App() {
   return (
     <>
       <Header>JOURNAL</Header>
-      <Main><Form submitText="Create"></Form></Main>
+      <main>
+        <Form submitText="Create"></Form>
+      </main>
       <Footer>Journal App - 2023</Footer>
     </>
   );
 }
-
-export default App;
-
-
-/*
-<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-*/
